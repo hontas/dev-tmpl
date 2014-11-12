fe9a485bc392ba9f7b4ab374c185fcd3d2bd16ad
Pontus Lundin <lundin.pontus@gmail.com>
2014-11-12 20:03:26 +0100

    updated remove step

--- utils/remove.js
+++ utils/remove.js
@@ -13,11 +13,12 @@ module.exports = function(answers) {
 	var deferred = Q.defer();
 
 	function done(answers) {
+		console.log(chalk.green('Uninstalled successfull'));
 		deferred.resolve(answers);
 	}
 
 	if (answers.cleanup) {
-		console.log(chalk.red('Uninstalling'), 'temporary packages', chalk.gray(Object.keys(dependencies)).join(' '));
+		console.log(chalk.red('Uninstalling'), 'temporary packages', chalk.gray(Object.keys(dependencies).join(' ')));
 		cmd('npm', ['uninstall'].concat(Object.keys(dependencies)))
 			.then(done, null, displayProgress);
 	} else {
