b284043505466a3b8bb616c0cd2ce4332c946d68
Pontus Lundin <lundin.pontus@gmail.com>
2014-11-18 17:23:13 +0100

    cleared all dependencies but karma

--- utils/dependencies.js
+++ utils/dependencies.js
@@ -1,22 +1,9 @@
 module.exports = {
 	npm: [
-		'gulp',
-		'chai',
-		'gulp',
-		'gulp-task-loader',
-		'gulp-less',
-		'gulp-concat',
-		'gulp-sourcemaps',
-		'gulp-changed',
-		'gulp-jshint',
-		'jshint-stylish',
 		'karma',
 		'karma-mocha',
 		'karma-sinon-chai',
 		'karma-nyan-reporter'
 	],
-	bower: [
-		'font-awesome',
-		'normalize-css'
-	]
+	bower: []
 };
